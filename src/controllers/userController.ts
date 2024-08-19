1692cb2c52c6ca31d268099d0a13dbf9ce8714be
Luis Felipe Ribeiro de Paula <fefedias2005@gmail.com>
2024-08-19 13:15:36 -0300

    Implementação das operações de atualização e exclusão para usuários pelo ID

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,21 +1,52 @@
 import { Request, Response, NextFunction } from "express";
 import * as userService from "../services/userService";
 
-export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const users = await userService.getUsers();
-        res.json(users);
-    } catch (error) {
-        next(error);
-    }
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await userService.getUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const {name, email} = req.body;
-        const newUser = await userService.createUser(name, email);
-        res.status(201).json(newUser);
-    } catch (error) {
-        next(error);
+export const upsertUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id, name, email } = req.body;
+    let user;
+
+    if (id) {
+      user = await userService.updateUser(Number(id), name, email);
+    } else {
+      user = await userService.createUser(name, email);
+      res.status(201).json(user);
+      return;
     }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id = Number(req.params.id);
+    await userService.deleteUser(id);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,17 @@
 import * as userModel from "../models/user";
 
 export const getUsers = async () => {
-    return await userModel.getUsers();
+  return await userModel.getUsers();
 };
 
 export const createUser = async (name: string, email: string) => {
-    return await userModel.createUser(name, email);
+  return await userModel.createUser(name, email);
+};
+
+export const updateUser = async (id: number, name: string, email: string) => {
+  return await userModel.updateUser(id, name, email);
+};
+
+export const deleteUser = async (id: number) => {
+  await userModel.deleteUser(id);
 };
